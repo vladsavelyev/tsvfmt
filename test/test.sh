e05e01066bd6897e2e6e45affddccde4b2691c12
vladsaveliev <vladislav.sav@gmail.com>
2021-01-05 22:42:40 +1100

    Update tests

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
-tsv test/test.tsv | grep -q "NA12891    -0.623         41  some random lines"
-cat test/test.tsv | tsv | grep -q "NA12891    -0.623         41  some random lines"
-tsv test/test.tsv.gz | grep -q "NA12891    -0.623         41  some random lines"
-tsv test/test.csv | grep -q "NA12891    -0.623         41  some random lines"
+tsv test/test.tsv | grep -q "NA12878   46  some longer line     11344  497"
+cat test/test.tsv | tsv | grep -q "NA12878   46  some longer line     11344  497"
+tsv -max 5 test/test.vcf.gz | grep -q 'chr1   9462$  .   G    A,AC\$  101.\$  .      DP=2\$'
+tsv test/test.csv | grep -q "NA12878    -0.865         36  some"
+tsv -d '|' test/pipe_del_file.txt | grep -q "NA12891 A  -0.623   41  some annotation"
